639cdc7b5d738d80ac9541f863d09fd46d96e943
kritika243 <24kritikasharma24@gmail.com>
2023-10-23 23:39:29 +0530

    :passport_control: user authentication controller functionality with email, password, and username

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto'
 
-const random = crypto.randomBytes(128).toString('base64')
+export const random = () => crypto.randomBytes(128).toString('base64')
 
 export const authentication = (salt: string, password: string) => {
   return crypto.createHmac('sha256', [salt, password].join('/')).update(process.env.SECRET).digest('hex')
